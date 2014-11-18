dbdc3f5bb58ee881e5e9c0e925e884e2bb5bebac
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-11-19 00:07:12 +0100

    + Added non working GUI elements (first draft)
    Done for today

--- S04 - Chat für Schwerhörige/src/stokic_taschner/gui/MultiCastChatMenu.java
+++ S04 - Chat für Schwerhörige/src/stokic_taschner/gui/MultiCastChatMenu.java
@@ -1,6 +1,10 @@
 package stokic_taschner.gui;
 
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.*;
+
 import javax.swing.*;
 
 /**
@@ -13,9 +17,11 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class MultiCastChatMenu extends JPanel implements ActionListener {
 	
+	JFrame f = new JFrame();
+	
 	public MultiCastChatMenu() {
 		
-		JFrame f = new JFrame();
+		
 		
 		this.addContents();
 		
@@ -29,18 +35,54 @@ public class MultiCastChatMenu extends JPanel implements ActionListener {
 	}
 	
 	public void addContents() {
-		JButton abc = new JButton("abc");
-		abc.addActionListener(this);
 		
-		this.add(abc);
+		JLabel l_hallo = new JLabel("Herzlich Willkommen");
+		
+		JLabel l_username = new JLabel("Benutzername");
+		JTextField tf_username = new JTextField();
+		
+		JLabel l_login = new JLabel("IP:Port");
+		JTextField tf_login = new JTextField();
+		
+		JButton b_connect = new JButton("Connect");
+		b_connect.addActionListener(this);
+		
+		JButton b_help = new JButton("Help");
+		b_help.addActionListener(this);
+		
+		
+		BoxLayout l = new BoxLayout(this, BoxLayout.Y_AXIS);
+		
+		
+		
+		this.setLayout(l);
+		
+		this.add(l_hallo);
+		this.add(new JPanel());
+		this.add(l_username);
+		this.add(tf_username);
+		this.add(new JPanel());
+		this.add(l_login);
+		this.add(tf_login);
+		this.add(new JPanel());
+		
+		this.add(b_connect);
+		this.add(b_help);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton b = (JButton) e.getSource();
 		
-		if (b.getText().equals("abc"))	{
-			System.out.println(b.getText());
+		
+		if (b.getText().equals("Connect"))	{
+			f.setVisible(false);
+			System.exit(0);
+		}
+		
+		if (b.getText().equals("Help"))	{
+			
+			System.exit(0);
 		}
 	}
 	
