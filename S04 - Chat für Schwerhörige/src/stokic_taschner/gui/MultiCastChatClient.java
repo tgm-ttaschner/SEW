8ea8fb03d93d2fe516fb48424dc48858067ce970
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-11-19 18:03:02 +0100

    + Added JScrollPane
    ~ GUI Elements now placed correctly

--- S04 - Chat für Schwerhörige/src/stokic_taschner/gui/MultiCastChatClient.java
+++ S04 - Chat für Schwerhörige/src/stokic_taschner/gui/MultiCastChatClient.java
@@ -12,8 +12,10 @@ public class MultiCastChatClient extends JPanel implements ActionListener	{
 
 	JPanel p_chat, p_input;
 
+	JScrollPane sp_output;
+
 	JTextField tf_input;
-	
+
 	JTextArea ta_output;
 
 	JButton b_send;
@@ -24,40 +26,37 @@ public class MultiCastChatClient extends JPanel implements ActionListener	{
 
 		this.addChatContents();
 
-		f_chat.setSize(320, 480);
+		f_chat.setSize(640, 480);
 		f_chat.setVisible(true);
 		f_chat.setLocationRelativeTo(null);
 		f_chat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f_chat.add(this);
 
 	}
-	
+
 	public void addChatContents()	{
-		
+
 		p_chat = new JPanel();
 		p_input = new JPanel();
-		
+
 		tf_input = new JTextField();
 		tf_input.setText("ewtpoughoizghogrsfi");
-		
+
 		ta_output = new JTextArea("reiwgffdsildsfh");
 		
+		sp_output = new JScrollPane(ta_output, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
 		b_send = new JButton("Senden");
-		
-		GridBagLayout lay_client = new GridBagLayout();
-		GridBagConstraints c = new GridBagConstraints();
-		
-		lay_client.addLayoutComponent(ta_output, c);
-		
+		b_send.addActionListener(this);
+
 		this.setLayout(new BorderLayout());
 		p_chat.setLayout(new BorderLayout());
 		p_input.setLayout(new BorderLayout());
-		
-		p_chat.add(ta_output);
-		
-		p_input.add(tf_input, BorderLayout.WEST);
-		p_input.add(b_send);
-		
+
+		p_chat.add(sp_output);
+		p_input.add(tf_input);
+		p_input.add(b_send, BorderLayout.EAST);
+
 		this.add(p_chat, BorderLayout.CENTER);
 		this.add(p_input, BorderLayout.SOUTH);
 	}
@@ -67,7 +66,7 @@ public class MultiCastChatClient extends JPanel implements ActionListener	{
 		JButton b = (JButton) e.getSource();
 
 		if (b.getText().equals("Senden"))	{
-			// Text von JTextField an Socket senden und im Fenster anzeigen lassen
+			ta_output.append(tf_input.getText() + "\n");
 		}
 	}
 }
