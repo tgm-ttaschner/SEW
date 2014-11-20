6ceaf73352cd0d433abbed8769132da409bbfc3a
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-11-20 21:23:24 +0100

    + Added commentary for Main

--- S04 - Chat für Schwerhörige/src/stokic_taschner/main/Main.java
+++ S04 - Chat für Schwerhörige/src/stokic_taschner/main/Main.java
@@ -2,7 +2,17 @@ package stokic_taschner.main;
 
 import stokic_taschner.gui.MultiCastChatMenu;
 
+/**
+ * @author Thomas Taschner
+ * @version 20.11.2014
+ * 
+ * Die Main Klasse des Programms (das auch die Main-Methode beinhaltet).
+ *
+ */
 public class Main {
+	/**
+	 * @param args Argumente von Main
+	 */
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		MultiCastChatMenu m = new MultiCastChatMenu();
