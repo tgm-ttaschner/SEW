7bfe7a86e9805bb415f40dfc40077ae2571b258c
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-11-20 21:26:30 +0100

    + Added commentary for ChatConnection

--- S04 - Chat für Schwerhörige/src/stokic_taschner/chat/ChatConnection.java
+++ S04 - Chat für Schwerhörige/src/stokic_taschner/chat/ChatConnection.java
@@ -1,7 +1,7 @@
 package stokic_taschner.chat;
 
 /**
- * Ein Interface fuer den MulticastChat. Beinhaltet das vorbereiten der MulticastConnection(username, 
+ * Ein Interface fuer den MulticastChat. Beinhaltet das Vorbereiten der MulticastConnection(username, 
  * hostname, port) und das Starten des MulticastChats.
  * 
  * @author Stefan Stokic
@@ -9,6 +9,17 @@ package stokic_taschner.chat;
  */
 public interface ChatConnection {
 
+	/**
+	 * @param username der Benutzername, der bei der Anmeldung verwendet werden soll
+	 * @param hostname der Name des Hosts, auf dem der MulticastSocket laufen soll
+	 * @param port der Port, auf dem der Socket laufen soll
+	 * 
+	 * Dient zum Herstellen der Verbindung zum Socket.
+	 */
 	public void setupChatConnection(String username, String hostname, int port);
+	
+	/**
+	 * Dient zum Starten des MulticastChats
+	 */
 	public void startChat();
 }
