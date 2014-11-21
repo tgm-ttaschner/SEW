b22621dda97a5e01e179ffa8e9b56588258299ad
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-11-21 18:15:17 +0100

    Message:
    + Added commentary

--- S04 - Chat für Schwerhörige/src/stokic_taschner/chat/Message.java
+++ S04 - Chat für Schwerhörige/src/stokic_taschner/chat/Message.java
@@ -1,18 +1,31 @@
 package stokic_taschner.chat;
 
+/**
+ * @author Stefan Stokic
+ * 
+ * Abstrakte Message Klasse, mit einer Getter- und Setter-Methode der zu sendende Nachricht
+ *
+ */
 public abstract class Message {
 
 	private String message;
 	
+	/**
+	 * @param message die zu sendende Nachricht
+	 */
 	public void setMessage(String message) {
-
 		this.message =  message;
 	}
 	
+	/**
+	 * @return die zu sendende Nachricht
+	 */
 	public String getMessage() {
-		
 		return this.message;
 	}
 	
+	/**
+	 * @return die zu sendende Nachricht
+	 */
 	public abstract String createMessage();
 }
