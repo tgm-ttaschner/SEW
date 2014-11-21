18aaa73bf0303ee8d03104243fa3ba47a7c7c392
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-11-21 17:51:42 +0100

    CapsedMessage:
    + Added commentary

--- S04 - Chat für Schwerhörige/src/stokic_taschner/chat/CapsedMessage.java
+++ S04 - Chat für Schwerhörige/src/stokic_taschner/chat/CapsedMessage.java
@@ -1,19 +1,40 @@
 package stokic_taschner.chat;
 
+/**
+ * @author Stefan Stokic
+ * @version 21.11.2014
+ * 
+ * Diese Klasse nimmt mit Hilfe des Decorator Patterns eine Nachricht entgegen und wandelt sie in Caps Lock Schreibweise um.
+ *
+ */
 public class CapsedMessage extends TypeMessage {
 
+	/**
+	 * @param message die zu veraendernde Nachricht
+	 * 
+	 * Konstruktor, der den Elternkonstruktor aufruft.
+	 */
 	public CapsedMessage(Message message) {
 		
 		super(message);
 	}
 	
+	/* (non-Javadoc)
+	 * @see stokic_taschner.chat.TypeMessage#createMessage()
+	 * 
+	 * Gibt eine Nachricht in Caps Lock Schreibweise zurueck
+	 */
 	public String createMessage() {
 		
 		return capsMessage(getTypeMessage().createMessage());
 	}
 	
+	
+	/**
+	 * @param message die urspruengliche Nachricht
+	 * @return die Nachricht in Caps Lock Schreibweise
+	 */
 	public String capsMessage(String message) {
-		
 		return message.toUpperCase();
 	}
 }
