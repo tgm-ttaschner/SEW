ae781fe82e51bebfc8909950ff87c64f1874d3b9
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-11-21 18:17:35 +0100

    TypeMessage:
    + Added commentary

--- S04 - Chat für Schwerhörige/src/stokic_taschner/chat/TypeMessage.java
+++ S04 - Chat für Schwerhörige/src/stokic_taschner/chat/TypeMessage.java
@@ -10,20 +10,36 @@ public abstract class TypeMessage extends Message {
 
 	private Message message;
 	
+	/**
+	 * @param message die Nachricht, die gesendet werden soll
+	 * 
+	 * Konstruktor, der die eigene Setter-Methode aufruft
+	 */
 	public TypeMessage(Message message) {
 		
 		this.setTypeMessage(message);
 	}
 	
+	/**
+	 * @param message die Nachricht, die gesendet werden soll
+	 */
 	public void setTypeMessage(Message message) {
 		
 		this.message = message;
 	}
 	
+	/**
+	 * @return die Nachricht, die gesendet werden soll
+	 */
 	public Message getTypeMessage() {
 		
 		return this.message;
 	}
 	
+	/* (non-Javadoc)
+	 * @see stokic_taschner.chat.Message#createMessage()
+	 * 
+	 * Gibt die zu sendende Nachricht zurueck
+	 */
 	public abstract String createMessage();
 }
