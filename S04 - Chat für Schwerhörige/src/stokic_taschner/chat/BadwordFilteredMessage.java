cfe502bdbc51af56281988d1257b1141b90c1cfa
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-11-21 17:44:53 +0100

    BadwordFilteredMessage:
    + Added some more commentary
    
    ~ Improved bad word detection
    
    - Removed charCount

--- S04 - Chat für Schwerhörige/src/stokic_taschner/chat/BadwordFilteredMessage.java
+++ S04 - Chat für Schwerhörige/src/stokic_taschner/chat/BadwordFilteredMessage.java
@@ -2,10 +2,24 @@ package stokic_taschner.chat;
 
 import java.util.*;
 
+/**
+ * @author Stefan Stokic
+ * @author Thomas Taschner
+ * @version 21.11.2014
+ * 
+ * Diese Klasse nimmt mit Hilfe des Decorator Patterns eine Nachricht entgegen, wandelt sie in Caps Lock Schreibweise um und zensiert aus einer Liste entnommene eventuell vorkommende Schimpfwoerter.
+ *
+ */
 public class BadwordFilteredMessage extends TypeMessage {
 
 	List<String> badwords = new ArrayList<String>();
 
+	/**
+	 * @param message die zu ueberpruefende Nachricht
+	 * 
+	 * Konstruktor, der den Elternkonstruktor aufruft.
+	 * Anschlieﬂend werden noch Schimpfwoerter einer Liste hinzugefuegt.
+	 */
 	public BadwordFilteredMessage(Message message) {
 
 		super(message);
@@ -14,25 +28,35 @@ public class BadwordFilteredMessage extends TypeMessage {
 		badwords.add("trottel");
 	}
 
+	/* (non-Javadoc)
+	 * @see stokic_taschner.chat.TypeMessage#createMessage()
+	 * 
+	 * Wendet den Filter an die groﬂgeschriebene Nachricht an
+	 */
 	public String createMessage() {
-
 		return applyFilter(getTypeMessage().createMessage().toUpperCase());
 	}
 
+	/**
+	 * @param message die zu zensierende Nachricht
+	 * @return die zensierte Nachricht
+	 * 
+	 * Die Nachricht wird in einzelne Woerter zerteilt, eventuell zensiert und wieder zusammengesetzt
+	 */
 	public String applyFilter(String message)	{
-		for (int i = 0; i < badwords.size(); i++)	{
-			if (message.equalsIgnoreCase(badwords.get(i)))	{
-				return message.replace(message, this.charCount(message.length()));
+
+		String s[] = message.split(" ");
+		
+		message = "";
+
+		for (int i = 0; i < s.length; i++)	{
+			for (int j = 0; j < badwords.size(); j++)	{
+				if (s[i].equalsIgnoreCase(badwords.get(j)))	{
+					s[i] = "$%&*";
+				}
 			}
+			message += s[i] + " ";
 		}
 		return message;
-	}
-
-	public String charCount(int length)	{
-		String s = "";
-		for (int i = 0; i < length; i++)	{
-			s += "*";
-		}
-		return s;
 	}
 }
