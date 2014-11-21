4a1641e483b15104af483fedd42fd041628589f0
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-11-21 18:14:56 +0100

    ChatMessage:
    + Added commentary

--- S04 - Chat für Schwerhörige/src/stokic_taschner/chat/ChatMessage.java
+++ S04 - Chat für Schwerhörige/src/stokic_taschner/chat/ChatMessage.java
@@ -1,10 +1,21 @@
 package stokic_taschner.chat;
 
+/**
+ * @author Stefan Stokic
+ * @version 21.11.2014
+ * 
+ * Diese Klasse ruft den Konstruktor in Message auf.
+ *
+ */
 public class ChatMessage extends Message {
 
+	/* (non-Javadoc)
+	 * @see stokic_taschner.chat.Message#createMessage()
+	 * 
+	 * Ruft die Getter-Methode von Message auf und gibt die zu sendende Nachricht zurück
+	 */
 	@Override
 	public String createMessage() {
-		
 		return super.getMessage();
 	}
 }
