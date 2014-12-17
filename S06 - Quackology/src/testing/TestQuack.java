1dba3c16168b11696a44a45f84f6d25340d49016
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-12-17 08:38:12 +0100

    Added some test cases

--- S06 - Quackology/src/testing/TestQuack.java
+++ S06 - Quackology/src/testing/TestQuack.java
@@ -2,13 +2,52 @@ package testing;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.*;
+
+import quack.*;
 
 public class TestQuack {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	EntenSimulator simulator;
+	AbstrakteEntenFabrik entenfabrik;
+	
+	Schar EntenSchar;
+	
+	@Before
+	public void setup()	{
+		simulator = new EntenSimulator();
+		entenfabrik = new ZaehlendeEntenFabrik();
+		EntenSchar = new Schar();
 	}
+	
+	@Test
+	public void testMoorEntenSchar() {
+		Quakfaehig moorEnte = entenfabrik.erzeugeMoorEnte();
+		EntenSchar.hinzufügen(moorEnte);
+		
+		Quakologe quakologe = new Quakologe();
+		EntenSchar.registriereBeobachter(quakologe);
 
+		simulator.simulieren(EntenSchar);
+		
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		//redirect the System-output (normaly the console) to a variable
+		System.setErr(new PrintStream(outContent));
+
+		//call your method here
+		String[] main = {};
+		EntenSimulator.main(main);
+
+		//check if your error message is in the output variable
+		assertEquals("Fehlerhafte Parameter!", outContent.toString());
+	}
+	
+	@Test
+	public void testLockPfeifenSchar() {
+		Quakfaehig lockPfeife = entenfabrik.erzeugeLockPfeife();
+		EntenSchar.hinzufügen(lockPfeife);
+	}
 }
