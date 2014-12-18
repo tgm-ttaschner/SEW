0776dc832a116fbbe311c8ba52aa060e576c5a11
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-12-18 21:21:34 +0100

    Added some more test cases, reaching 100 % now

--- S06 - Quackology/src/testing/TestQuack.java
+++ S06 - Quackology/src/testing/TestQuack.java
@@ -8,197 +8,295 @@ import org.junit.*;
 
 import quack.*;
 
+/**
+ * @author Thomas Taschner
+ * @version 18.12.2014
+ * 
+ * Testet die Entenfabrik auf ihre Funktionalitaet.
+ *
+ */
 public class TestQuack extends MoorEnte {
-	
+
 	EntenSimulator simulator;
 	AbstrakteEntenFabrik entenfabrik;
-	
+
 	Schar EntenSchar;
-	
+
+	/**
+	 * Vorbereitend fuer die Tests
+	 */
 	@Before
 	public void setup()	{
 		simulator = new EntenSimulator();
 		entenfabrik = new ZaehlendeEntenFabrik();
 		EntenSchar = new Schar();
 	}
-	
+
+	/**
+	 * Eine Moorentenschar quakt.
+	 */
 	@Test
 	public void testMoorEntenSchar()	{
-		
+
 		Quakfaehig moorEnte = entenfabrik.erzeugeMoorEnte();
-		EntenSchar.hinzufügen(moorEnte);
-		
+		EntenSchar.hinzufuegen(moorEnte);
+
 		Quakologe quakologe = new Quakologe();
 		EntenSchar.registriereBeobachter(quakologe);
-		
+
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		//redirect the System-output (normaly the console) to a variable
 		System.setOut(new PrintStream(outContent));
-		
-		//simulator.simulieren(EntenSchar);
 
 		//call your method here
 		simulator.simulieren(moorEnte);
-		
+
 		// Es gab Probleme mit dem Lineseparator
 		String abc = "Quak" + System.lineSeparator() + "Quakologe: Moorente hat gerade gequakt." + System.lineSeparator();
 
 		//check if your error message is in the output variable
 		assertEquals(abc, outContent.toString());
 	}
-	
+
+	/**
+	 * Eine Lockpfeifenschar 'kwaakt'.
+	 */
 	@Test
 	public void testLockPfeifenSchar() {
 		Quakfaehig lockPfeife = entenfabrik.erzeugeLockPfeife();
-		EntenSchar.hinzufügen(lockPfeife);
-		
+		EntenSchar.hinzufuegen(lockPfeife);
+
 		Quakologe quakologe = new Quakologe();
 		EntenSchar.registriereBeobachter(quakologe);
-		
+
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		//redirect the System-output (normaly the console) to a variable
 		System.setOut(new PrintStream(outContent));
-		
-		//simulator.simulieren(EntenSchar);
 
 		//call your method here
 		simulator.simulieren(lockPfeife);
-		
+
 		// Es gab Probleme mit dem Lineseparator
 		String abc = "Kwaak" + System.lineSeparator() + "Quakologe: Lockpfeife hat gerade gequakt." + System.lineSeparator();
 
 		//check if your error message is in the output variable
 		assertEquals(abc, outContent.toString());
-		
-		
+
+
 	}
-	
+
+	/**
+	 * Eine Gummientenschar quietscht.
+	 */
 	@Test
 	public void testGummiEntenSchar() {
 		Quakfaehig gummiEnte = entenfabrik.erzeugeGummiEnte();
-		EntenSchar.hinzufügen(gummiEnte);
-		
+		EntenSchar.hinzufuegen(gummiEnte);
+
 		Quakologe quakologe = new Quakologe();
 		EntenSchar.registriereBeobachter(quakologe);
-		
+
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		//redirect the System-output (normaly the console) to a variable
 		System.setOut(new PrintStream(outContent));
-		
-		//simulator.simulieren(EntenSchar);
 
 		//call your method here
 		simulator.simulieren(gummiEnte);
-		
+
 		// Es gab Probleme mit dem Lineseparator
 		String abc = "Quietsch" + System.lineSeparator() + "Quakologe: Gummiente hat gerade gequakt." + System.lineSeparator();
 
 		//check if your error message is in the output variable
 		assertEquals(abc, outContent.toString());
-		
-		
+
+
 	}
-	
+
+	/**
+	 * Eine Gansadapterschar schnattert.
+	 */
 	@Test
 	public void testGansAdapterSchar() {
 		Quakfaehig gansEnte = new GansAdapter(new Gans());
-		EntenSchar.hinzufügen(gansEnte);
-		
+		EntenSchar.hinzufuegen(gansEnte);
+
 		Quakologe quakologe = new Quakologe();
 		EntenSchar.registriereBeobachter(quakologe);
-		
+
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		//redirect the System-output (normaly the console) to a variable
 		System.setOut(new PrintStream(outContent));
-		
+
 		//simulator.simulieren(EntenSchar);
 
 		//call your method here
 		simulator.simulieren(gansEnte);
-		
+
 		// Es gab Probleme mit dem Lineseparator
 		String abc = "Schnatter" + System.lineSeparator() + "Quakologe: sich als Ente ausgebende Gans hat gerade gequakt." + System.lineSeparator();
 
 		//check if your error message is in the output variable
 		assertEquals(abc, outContent.toString());
-		
-		
+
+
 	}
-	
+
+	/**
+	 * Eine Stockentenschar quakt.
+	 */
 	@Test
 	public void testStockEntenSchar() {
 		Quakfaehig stockEnte = entenfabrik.erzeugeStockEnte();
-		EntenSchar.hinzufügen(stockEnte);
-		
+		EntenSchar.hinzufuegen(stockEnte);
+
 		Quakologe quakologe = new Quakologe();
 		EntenSchar.registriereBeobachter(quakologe);
-		
+
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		//redirect the System-output (normaly the console) to a variable
 		System.setOut(new PrintStream(outContent));
-		
-		//simulator.simulieren(EntenSchar);
 
 		//call your method here
 		simulator.simulieren(stockEnte);
-		
+
 		// Es gab Probleme mit dem Lineseparator
 		String abc = "Quak" + System.lineSeparator() + "Quakologe: Stockente hat gerade gequakt." + System.lineSeparator();
 
 		//check if your error message is in the output variable
 		assertEquals(abc, outContent.toString());
-		
-		
+
+
 	}
-	
+
+	/**
+	 * Der Quakologe gibt seine eigene Bezeichnung zurueck.
+	 */
 	@Test
 	public void testQuakologeToString() {
 		Quakologe q = new Quakologe();
 		assertEquals("Quakologe", q.toString());
 	}
 	
+	/**
+	 * Die Gans gibt ihre eigene Bezeichnung zurueck.
+	 */
 	@Test
 	public void testGansToString() {
 		Gans g = new Gans();
 		assertEquals("Gans", g.toString());
 	}
-	
+
+	/**
+	 * Registiert eine Moorente beim Senderring und ueberprueft, ob diese registriert wurde.
+	 */
 	@Test
 	public void testSenderRingGetBeobachtende()	{
-		
-		SenderRing s = new SenderRing(new MoorEnte());
-		
+
+		SenderRing s = new SenderRing(entenfabrik.erzeugeMoorEnte());
+
 		s.registriereBeobachter(new Quakologe());
 		
-		System.out.println(s.getBeobachtende().next().toString());
-
-		//check if your error message is in the output variable
 		assertNotNull(s.getBeobachtende().next());
 	}
-	
+
+	/**
+	 * Check der korrekten Zaehlweise des Quackzaehlers.
+	 */
 	@Test
 	public void testQuackZaehler()	{
-		
-		Quakfaehig moorEnte = entenfabrik.erzeugeMoorEnte();
-		//EntenSchar.hinzufügen(moorEnte);
-		
-		//Quakologe quakologe = new Quakologe();
-		//EntenSchar.registriereBeobachter(quakologe);
-		
-		//call your method here
-		//simulator.simulieren(moorEnte);
-		
-		
-		
-		QuakZaehler z = new QuakZaehler(moorEnte);
-		
-		//z.quaken();
-		//z.quaken();
-
-		//check if your error message is in the output variable
-		assertEquals(1, QuakZaehler.getQuaks());
+		assertEquals(10, QuakZaehler.getQuaks());
 	}
 	
+	/**
+	 * Fuegt dem QuackZaehler eine Moorente hinzu, laesst allen Beobachtern ihren eigenen Typ zukommen und gibt diesen dann aus.
+	 */
+	@Test
+	public void testQuackZaehlerBenachrichtigeBeobachtende()	{
+		QuakZaehler q = new QuakZaehler(new MoorEnte());
+		q.benachrichtigeBeobachtende();
+		assertEquals("Moorente", q.toString());
+	}
+	
+	/**
+	 * Benachrichtigt alle beobachtenden Scharen.
+	 */
+	@Test
+	public void testScharBenachrichtigeBeobachtende()	{
+		EntenSchar = new Schar();
+		EntenSchar.benachrichtigeBeobachtende();
+	}
+	
+	/**
+	 * Gibt den Typ der Schar zurueck.
+	 */
+	@Test
+	public void testScharToString()	{
+		EntenSchar = new Schar();
+		EntenSchar.hinzufuegen(new MoorEnte());
+		
+		assertEquals("Entenschar", EntenSchar.toString());
+	}
+	
+	/**
+	 * Ueberprueft, ob die Entenfabrik auch wirklich eine Stockente erzeugt.
+	 */
+	@Test
+	public void testEntenfabrikErzeugeStockEnte()	{
+		Quakfaehig q = new Entenfabrik().erzeugeStockEnte();
+		
+		if (q instanceof StockEnte) {
+			assertTrue(true);
+		}
+	}
+	
+	/**
+	 * Ueberprueft, ob die Entenfabrik auch wirklich eine Moorente erzeugt.
+	 */
+	@Test
+	public void testEntenfabrikErzeugeMoorEnte()	{
+		Quakfaehig q = new Entenfabrik().erzeugeMoorEnte();
+		
+		if (q instanceof MoorEnte) {
+			assertTrue(true);
+		}
+	}
+	
+	/**
+	 * Ueberprueft, ob die Entenfabrik auch wirklich eine Lockpfeife erzeugt.
+	 */
+	@Test
+	public void testEntenfabrikErzeugeLockPfeife()	{
+		Quakfaehig q = new Entenfabrik().erzeugeLockPfeife();
+		
+		if (q instanceof LockPfeife) {
+			assertTrue(true);
+		}
+	}
+	
+	/**
+	 * Ueberprueft, ob die Entenfabrik auch wirklich eine Gummiente erzeugt.
+	 */
+	@Test
+	public void testEntenfabrikErzeugeGummiEnte()	{
+		Quakfaehig q = new Entenfabrik().erzeugeGummiEnte();
+		
+		if (q instanceof GummiEnte) {
+			assertTrue(true);
+		}
+	}
+	
+	
+	/**
+	 * Fuehrt die Entensimulation aus (hauptsaechlich zu Coveragezwecken).
+	 */
+	@Test
+	public void testEntenSimulator()	{
+		EntenSimulator.main(null);
+	}
+
+	/**
+	 * Zuruecksetzen der Konsolenausgabe
+	 */
 	@After
 	public void cleanup()	{
 		System.setErr(null);
